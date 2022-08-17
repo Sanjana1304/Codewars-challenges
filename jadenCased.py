cbb742dd376f5152a6288edf9988cc3325b6cef4
Sanjana Acharya <64159652+Sanjana1304@users.noreply.github.com>
2022-08-17 09:41:17 +0530

    Update jadenCased.py

--- jadenCased.py
+++ jadenCased.py
@@ -6,6 +6,7 @@ Example:
 Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
 Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
 
+LEVEL : 7kyu
 """
 def to_jaden_case(string):
     string=string.split()
